6c854c8431af9cd62d71e5ddd38e16611bec5d9b
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-21 14:35:43 +0900

    Update

--- module/s3.tf
+++ module/s3.tf
@@ -2,7 +2,7 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "s3" {
   bucket = "example-bucket-${random_id.example.hex}"
   # acl    = var.acl_enabled ? "private" : "public-read" # ACL을 활성화 또는 비활성화합니다.
 

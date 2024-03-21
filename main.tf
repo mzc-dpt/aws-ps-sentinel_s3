5896fec64a7d857b66fed7265f5642022067d1f4
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-21 14:14:43 +0900

    Update

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
-resource "random_id" "example" {
-  byte_length = 8
-}
-
-
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket-${random_id.example.hex}"
-  acl    = var.acl_enabled ? "private" : "public-read" # ACL을 활성화 또는 비활성화합니다.
-
-  tags = {
-    Name        = "ExampleBucket"
-    Environment = "Production"
-  }
+module "s3" {
+  source                     = "./"
+  s3_acl                     = "private"
+  s3_ownership               = "BucketOwnerPreferred"
+  s3_block_public_acls       = true
+  s3_block_public_policy     = true
+  s3_ignore_public_acls      = true
+  s3_restrict_public_buckets = true
+  create_kms_for_s3          = false
+  s3_sse_algorithm           = "aws:kms"
 }

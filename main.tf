1d58dcd22ba00f537df1db3a237744ec037a75b1
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-22 16:35:59 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket_acl" "example" {
   depends_on = [aws_s3_bucket_ownership_controls.example]
 
   bucket = aws_s3_bucket.example.id
-  acl    = var.acl_enabled
+  acl    = var.acl_enabled ? "private" : "public-read"
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
@@ -30,7 +30,22 @@ resource "aws_s3_bucket_public_access_block" "example" {
 }
 
 variable "acl_enabled" {
-  type    = string
-  default = "public-read"
+  type    = bool
+  default = false
 }
  
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}

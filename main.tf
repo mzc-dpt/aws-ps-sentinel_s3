48ba31d31bc2b8122cabd696f44f2882acb8af04
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-26 16:06:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-resource "random_id" "example" {
+resource "random_id" "bucket-suffix" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "s3-bucket-sentinel-${random_id.example.hex}"
+resource "aws_s3_bucket" "s3-bucket-sentinel" {
+  bucket = "s3-bucket-sentinel-${random_id.bucket-suffix.hex}"
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_ownership_controls" "s3-bucket-ownership" {
+  bucket = aws_s3_bucket.s3-bucket-sentinel.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
+resource "aws_s3_bucket_acl" "s3-bucket-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3-bucket-ownership]
 
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.s3-bucket-sentinel.id
   acl    = var.acl_enabled ? "private" : "public-read"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "s3-bucket-public-access" {
+  bucket = aws_s3_bucket.s3-bucket-sentinel.id
 
   block_public_acls       = false
   block_public_policy     = false
